01663ddd9482f51ff58b1cbf3523dbd6e09138cb
ivana.tanova <tanovait@gmail.com>
2020-04-21 17:25:20 +0300

    List All Vets on Vets Page Closes #14.

--- pet-clinic-web/src/main/java/com/tanovait/springpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/tanovait/springpetclinic/controllers/VetsController.java
@@ -1,14 +1,22 @@
 package com.tanovait.springpetclinic.controllers;
 
-import org.springframework.context.annotation.Conditional;
+import com.tanovait.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets", "vets/index", "vets/index.html"})
-    String listVets(){
+    String listVets(Model model){
+        model.addAttribute("vets", vetService.getAll());
         return "vets/index";
     }
 }
