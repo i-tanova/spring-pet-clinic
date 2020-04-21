efa7e09d9e1ee7cbe2e8bef8727541f9de03e3ee
ivana.tanova <tanovait@gmail.com>
2020-04-21 17:52:19 +0300

    Update Map Based Service to Autogenerate Ids Closes #22

--- pet-clinic-web/src/main/java/com/tanovait/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tanovait/springpetclinic/bootstrap/DataLoader.java
@@ -22,22 +22,18 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner = new Owner("FirstOwner", "SecondNameFirstOwner");
-        owner.setId(1L);
         ownerService.save(owner);
         System.out.println("Owner service 1 data saved");
 
         Owner owner1 = new Owner("SecondOwner", "LastNameSecondOwner");
-        owner1.setId(2L);
         ownerService.save(owner1);
         System.out.println("Owner service 2 data saved");
 
         Vet vet = new Vet("FistVet", "FistVetSecondName");
-        vet.setId(1L);
         vetService.save(vet);
         System.out.println("Vet service 1 data saved");
 
         Vet vet1 = new Vet("SecondVet", "SecondVetSecondName");
-        vet1.setId(2L);
         vetService.save(vet1);
         System.out.println("Vet service 2 data saved");
     }
