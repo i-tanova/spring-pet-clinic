82498ec73fe8d55add20d232e65979ea7b5382ca
ivana.tanova <tanovait@gmail.com>
2020-04-21 17:22:34 +0300

    List all owners. Closes #15

--- pet-clinic-data/src/main/java/com/tanovait/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/tanovait/springpetclinic/model/Person.java
@@ -2,6 +2,23 @@ package com.tanovait.springpetclinic.model;
 
 public class Person extends BaseEntity {
     private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     private String lastName;
 
     public Person(String firstName, String lastName) {
