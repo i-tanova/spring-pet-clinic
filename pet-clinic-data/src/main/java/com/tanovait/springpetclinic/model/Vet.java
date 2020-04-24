bd56c2c6bbc3a94ae4850bd2ac31fc9028a93e0e
ivana.tanova <tanovait@gmail.com>
2020-04-24 10:42:05 +0300

    Create Vet Speciality Entity, Associate to Vet Closes #26

--- pet-clinic-data/src/main/java/com/tanovait/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/tanovait/springpetclinic/model/Vet.java
@@ -1,8 +1,20 @@
 package com.tanovait.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    private Set<Speciality> specialties;
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
     }
 }
