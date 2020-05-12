8394d37614abf0f2b9fe1106942b59ca23a35374
ivana.tanova <tanovait@gmail.com>
2020-05-12 08:00:09 +0300

    Create Spring Data JPA Service for Pet Types Closes #41

---
+++ pet-clinic-data/src/main/java/com/tanovait/springpetclinic/springdatajpa/PetTypeJpaService.java
@@ -0,0 +1,48 @@
+package com.tanovait.springpetclinic.springdatajpa;
+
+import com.tanovait.springpetclinic.model.PetType;
+import com.tanovait.springpetclinic.repositories.PetTypeRepository;
+import com.tanovait.springpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public Set<PetType> getAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public void delete(PetType object) {
+       petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+       petTypeRepository.deleteById(aLong);
+    }
+}
