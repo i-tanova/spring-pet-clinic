bd86cac4e216e4d993aa33e6e94b3be9d91450d3
ivana.tanova <tanovait@gmail.com>
2020-05-14 08:20:12 +0300

    Create Visit Map Service Closes #45

---
+++ pet-clinic-data/src/main/java/com/tanovait/springpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.tanovait.springpetclinic.services;
+
+import com.tanovait.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/tanovait/springpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.tanovait.springpetclinic.services.map;
+
+import com.tanovait.springpetclinic.model.Vet;
+import com.tanovait.springpetclinic.model.Visit;
+import com.tanovait.springpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> getAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
